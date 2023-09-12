b2615346476379514f777fd371647acba2bc5e66
MMotionMan <114028585+KirichenkoAV22VT191@users.noreply.github.com>
2023-09-13 01:51:40 +0300

    E complete

--- yandexAlgoTrain/3HW/E.py
+++ yandexAlgoTrain/3HW/E.py
@@ -1,21 +1,9 @@
-def count_similarity(arr1, arr2):
-    s = 0
-    for item1 in arr1:
-        if item1 in arr2:
-            s += 1
-
-    if abs(len(arr1) - len(arr2)) == len(arr2) - s:
-        return True
-    else:
-        return False
-
-
 m = int(input())
 
 witnesses = []
 
 for i in range(m):
-    witnesses.append(list(map(str, input().strip())))
+    witnesses.append(set(map(str, input().strip())))
 
 n = int(input())
 
@@ -25,13 +13,15 @@ counter = [0 for j in range(n)]
 
 for i in range(n):
     criminals.append(list(map(str, input().strip())))
-    for j in range(m):
-        if count_similarity(witnesses[j], criminals[i]):
-            counter[i] += 1
+    t_criminals = set(criminals[i])
 
+    for j in range(m):
+        if len(t_criminals & witnesses[j]) == len(witnesses[j]):
+            counter[i] += 1
 
 maximum = max(counter)
 
 for i in range(len(counter)):
     if counter[i] == maximum:
         print(''.join(criminals[i]))
+
